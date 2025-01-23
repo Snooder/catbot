839a94ee8c1973f1d2277ebf55844f26589678da
Matt Snyder <matt.snyder@yahoo.com>
2025-01-23 11:18:33 -0500

    Update catbot.py

--- local_llama/catbot.py
+++ local_llama/catbot.py
@@ -3,4 +3,4 @@ import ollama
 while True:
     user_input = input("You: ")
     response = ollama.chat(model="mistral", messages=[{"role": "user", "content": user_input}])
-    print(f"Bot: {response['message']['content']}")
+    print(f"Bot: {response['message']['content']}\n")
